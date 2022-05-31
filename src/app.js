c617af048aeecb0276642ab78fb54685ca2f7e52
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-05-31 16:56:37 -0300

    change frase

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import express from "express";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js"
 
-db.on("error", console.log.bind(console, 'erro de conexão'));
+db.on("error", console.log.bind(console, 'error connection'));
 db.once("open", () => {
-    console.log("conexão com o banco feita!")
+    console.log("conection with bank is active!")
 })
 
 const app = express();
